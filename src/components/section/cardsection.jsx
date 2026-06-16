9956bea2b040f9da8e133334d5fceb4c5f76d2c2
Parth Gohil <parthsinhgohil01@gmail.com>
2026-06-16 18:57:50 +0530

    add cardsection file

---
+++ src/components/section/cardsection.jsx
@@ -0,0 +1,20 @@
+// src/components/section/cardsection.jsx
+// Import the new ProductSwiper component from your card file
+import { ProductSwiper } from '../shoppingcard'; 
+
+export default function Slidercard() {
+  return (
+    <div className="w-full absolute bg-transparent text-white min-h-[500px]">
+     
+       {/* Section Header Title */}
+            <div className="w-full flex max-w-7xl mx-auto text-center mb-10 px-4">
+                <h2 className="text-3xl text-left md:text-4xl font-bold tracking-tight text-black font-sans">
+                    Transforming Surfaces with Spray Mastery
+                </h2>
+            </div>
+        <ProductSwiper />
+        
+      </div>
+
+  );
+}
